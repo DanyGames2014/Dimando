d50eb507cd1278e596750d5d3bb20329cf0cbe56
Daniel Fildán <fildand@centrum.cz>
2023-09-25 13:45:38 +0200

    Update DimandoOreStructure.java

--- src/main/java/net/danygames2014/dimando/world/DimandoOreStructure.java
+++ src/main/java/net/danygames2014/dimando/world/DimandoOreStructure.java
@@ -10,27 +10,32 @@ import java.util.Random;
 public class DimandoOreStructure extends Structure {
     @Override
     public boolean generate(Level world, Random random, int x, int y, int z) {
-        int richness = random.nextInt(0,10);
+        int richness = random.nextInt(0, 10);
 
-        // POOR
-        if(richness <= 4){
-            world.setTileInChunk(x,y,z, BlockListener.poor_dimando_ore.id);
-            return true;
+        switch (richness) {
+            case 9:
+            case 8:
+                world.setTileInChunk(x, y, z, BlockListener.rich_dimando_ore.id);
+                return true;
 
-        // NORMAL
-        } else if (richness <= 8) {
-            world.setTileInChunk(x,y,z, BlockListener.dimando_ore.id);
-            return true;
+            case 7:
+            case 6:
+            case 5:
+                world.setTileInChunk(x, y, z, BlockListener.dimando_ore.id);
+                return true;
 
-        // RICH
-        } else if (richness == 9) {
-            world.setTileInChunk(x,y,z, BlockListener.rich_dimando_ore.id);
-            return true;
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+            case 0:
+                world.setTileInChunk(x, y, z, BlockListener.poor_dimando_ore.id);
+                return true;
 
-        // WHAT?!
-        } else{
-            Dimando.logger.error("Richness was " + richness + ", this shouldn't happen");
-            return false;
+            default:
+                Dimando.logger.error("Richness was " + richness + ", this shouldn't happen");
+                return false;
+
         }
     }
 }
